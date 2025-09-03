7b206cd85c191dadcb689937edb0fd2b0cd1cc71
AndreAllegrini <a.allegrini@ucl.ac.uk>
2025-09-03 11:52:45 +0100

    cpus

--- scripts/subLDpred2.sh
+++ scripts/subLDpred2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --output=scoring.LDpred2.array_%A_%a.out
 #SBATCH --error=scoring.LDpred2.array_%A_%a.err
-#SBATCH --time=6:00:00
-#SBATCH --cpus-per-task=32
+#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --array=1-2
 
