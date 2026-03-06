6fcccf45c5c58ba8237c483d6bb3025023dc86a2
AndreAllegrini <a.allegrini@ucl.ac.uk>
2026-03-06 13:42:27 +0000

    updated pths

--- scripts/subLDpred2.sh
+++ scripts/subLDpred2.sh
@@ -1,24 +1,48 @@
 #!/bin/bash -l
-#SBATCH --output=scoring.LDpred2.array_%A_%a.out
-#SBATCH --error=scoring.LDpred2.array_%A_%a.err
-#SBATCH --time=4:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --array=1-2
+#$ -S /bin/bash
+#$ -l h_rt=06:00:00
+#$ -l mem=8G
+#$ -l tmpfs=8G
+#$ -pe smp 8
+#$ -t 1-16
+#$ -N subLDpred2
+#$ -m be
 
+# Limit threads for reproducibility
 export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
+export R_LIBS=~/Rlibs
 
-## Set up job enviroment:
-source /cluster/bin/jobsetup
-module purge
-module add R/3.5.0
+# Load R
+module -f unload compilers mpi gcc-libs
+module load r/recommended
 
+# Stop on error
 set -o errexit
 
-echo I am job $SLURM_JOBID
+# Working directory
+WORKDIR="/path/to/wd"
+cd "$WORKDIR"
 
-sumstat=$(awk -F',' "NR==$SLURM_ARRAY_TASK_ID {print \$1}" ${WORKdir}/GWAS.csv) #take first column of input file
-type=$(awk -F',' "NR==$SLURM_ARRAY_TASK_ID {print \$2}" ${WORKdir}/GWAS.csv) #take second column of input file
+echo "Running job $JOB_ID in $PWD for SGE task $SGE_TASK_ID"
 
-Rscript --vanilla ldpred2_auto_inf_qc.r -s $sumstat -t $type 
+# Get sumstat and type from CSV
+SUMSTAT=$(awk -F',' "NR==$SGE_TASK_ID {print \$1}" "${WORKDIR}/sumstats_list.csv")
+TYPE=$(awk -F',' "NR==$SGE_TASK_ID {print \$2}" "${WORKDIR}/sumstats_list.csv")
+
+# Paths
+LDREF="/path/to/ldref"          # LD reference
+GENOFILE="/path/to/genofile.rds" #  bigsnpr object
+OUTFILE="/path/to/output"        # out dir
+SUMSTATDIR="/path/to/sumstats"   # sumstats dir
+
+# Run LDpred2
+Rscript --vanilla ldpred2_auto_inf_qc_lift2.r \
+  -s "$SUMSTAT" \
+  -t "$TYPE" \
+  -m "$LDREF" \
+  -l hg19 \
+  -g "$GENOFILE" \
+  -o "$OUTFILE" \
+  -c 8 \
+  -d "$SUMSTATDIR"
